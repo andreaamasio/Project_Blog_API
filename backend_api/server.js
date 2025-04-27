e25e20506222d3995d21489dca508f2690dacf12
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-04-27 11:20:56 +0200

    fixed server.js with all imports

--- backend_api/server.js
+++ backend_api/server.js
@@ -1,7 +1,10 @@
 require("dotenv").config()
 const cors = require("cors")
 const express = require("express")
-// ... other imports ...
+const jwt = require("jsonwebtoken")
+const blogRouter = require("./routers/blogRouter")
+const userRouter = require("./routers/userRouter")
+const commentRouter = require("./routers/commentRouter")
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -27,5 +30,10 @@ app.use(cors(corsOptions))
 app.use("/blog", blogRouter)
 app.use("/comment", commentRouter)
 app.use("/user", userRouter)
+app.use("/", (req, res) => {
+  res.json({
+    message: "Welcome to the blog API!",
+  })
+})
 const port = process.env.PORT || 3000 // Use process.env.PORT for Render
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
